caa9f2f23b4d1170953f356a4d67255119d4f229
Jefferson Oliveira <jefferson.tecmec@hotmail.com>
2023-05-16 09:35:48 -0300

    Feito a criação do usuario e validação de login

--- scr/index.js
+++ scr/index.js
@@ -1,3 +1,4 @@
+import { hash } from 'bcrypt';
 import express, { request, response } from 'express'
 const app = express();
 
@@ -9,20 +10,38 @@ let users = [];
 
 app.post("/user", (request,response)=>{
     const user = request.body
+    const userexist = users.find(user =>user.email === request.body.email)
+
+    if(userexist){
+        return response.status(401).send("Email ja existe em outro usuario")
+    }else{
+        const bcryptPassword = bcrypt.hash(password,10)
     users.push({
         
             id: Math.floor(Math.random()*67676),
             name: user.name,
             email: user.email,
-            password: user.password,
-            message: {
-                messageId: Math.floor(Math.random()*67),
-                titulo: user.message.titulo,
-                descricao: user.message.descricao
-            }
+            bcryptPassword:hash,
+            note:[]
     })
-    response.status(204).json();
+    response.status(200).send("Usuario criado com sucesso!");
     console.log(user)
+    }
+})
+app.post("/user", (request,response)=>{
+    const user = request.body
+    const userexist = users.find(user =>user.email === request.body.email)
+
+    if(!userexist.name){
+        return response.status(401).send("Nome de usuario nao encontrado")
+    }
+    if(!userexist.password){
+        return response.status(401).send("Senha esta incorreta")
+    }
+    else{
+    response.status(200).send("Usuario Logado com sucesso!");
+    
+    }
 })
 app.get("/user",(request,response)=>{
 
@@ -39,15 +58,10 @@ app.put("/user/:id",(request,response)=>{
     const indexuser = users.findIndex(cuser=> user.id === id)
     users[indexuser]= {
         
-        id: Math.floor(Math.random()*67676),
-        name: user.name,
-        email: user.email,
-        password: user.password,
-        message: {
-            messageId: Math.floor(Math.random()*67),
-            titulo: user.message.titulo,
-            descricao: user.message.descricao
-             }
+        note:[ {
+                titulo: user.note.titulo,
+                descricao: user.note.descricao
+            }]
             }
         console.log(user[indexuser])
         return response.status(200).json(user[indexuser])
