c3f6968b8ae9e431e0b4f49323e9417491f438df
Jefferson Oliveira <jefferson.tecmec@hotmail.com>
2023-05-17 13:50:47 -0300

    Corrigido Login do usuario

--- scr/index.js
+++ scr/index.js
@@ -1,4 +1,4 @@
-import { hash } from 'bcrypt';
+import bcrypt from 'bcrypt';
 import express, { request, response } from 'express'
 const app = express();
 
@@ -12,73 +12,75 @@ app.post("/user", (request,response)=>{
     const user = request.body
     const userexist = users.find(user =>user.email === request.body.email)
     const  saltRounds = 10;
-    bcrypt.hash(user.password,saltRounds,(err,hash)=>{
-        if(hash){
-            users.push({
-        
-                id: Math.floor(Math.random()*67676),
-                name: user.name,
-                email: user.email,
-                password:hash,
-                note:[]
-        })
-        }else {
-            return response.status(400).send("Ocorreu um erro"+ err)
-        }
-    })
 
     if(userexist){
-        return response.status(401).send("Email ja existe em outro usuario")
+        return response.status(401).json("Email ja existe em outro usuario")
     }else{
-        
+        bcrypt.hash(user.password,saltRounds,function(err,hash){
+            if(hash){
+                users.push({
+            
+                    id: Math.floor(Math.random()*67676),
+                    name: user.name,
+                    email: user.email,
+                    password:hash,
+                    note:[]
+            })
+            }else {
+                return response.status(400).json("Ocorreu um erro"+ err)
+            }
+        })
     
-    response.status(204).send("Usuario criado com sucesso!");
+    response.status(204).json("Usuario criado com sucesso!");
     console.log(user)
     }
 })
-app.post("/user", (request,response)=>{
-    const user = request.body
-    const userexist = users.find(user =>user.email === request.body.email)
+app.post("/userLogin", (request,response)=>{
+    
+    const username = users.find(user =>user.name === request.body.name)
+    const userpassword = users.find(user =>user.password === request.body.password)
 
-    if(!userexist.name){
-        return response.status(401).send("Nome de usuario nao encontrado")
+    if(!username.name){
+        return response.status(401).json("Nome de usuario nao encontrado")
     }
-    if(!userexist.password){
-        return response.status(401).send("Senha esta incorreta")
+    if(!userpassword.password){
+        return response.status(401).json("Senha esta incorreta")
     }
     else{
-    response.status(200).send("Usuario Logado com sucesso!");
+    response.status(200).json("Usuario Logado com sucesso!");
     
     }
 })
-app.get("/user",(request,response)=>{
+app.get("/user",(request, response)=>{
+    
+    return response.status(200).json(users);
 
-    return response.status(200).json(users)
 })
-app.get("/user/:id",(request, response)=>{
+
+app.get("/userNote/:id",(request, response)=>{
     const id = Number(request.params.id);
     const user = users.find(user=> user.id===id);
     return response.json(user);
 })
-app.put("/user/:id",(request,response)=>{
+app.put("/userNote/:id/",(request,response)=>{
     const user = request.body
     const id =Number(request.params.id)
-    const indexuser = users.findIndex(cuser=> user.id === id)
-    users[indexuser]= {
+    const indexuser = users.findIndex(user=> user.id === id)
+    users[indexuser]= 
         
-        note:[{
+        users.note.push({
+                noteid: Math.floor(Math.random()*67),
                 titulo: user.note.titulo,
                 descricao: user.note.descricao
-            }]
-            }
+            })
         console.log(user[indexuser])
         return response.status(200).json(user[indexuser])
  })
 
-app.delete("/cars/:id",(request,response)=>{
+app.delete("/userDelete/:id",(request,response)=>{
     const id =Number(request.params.id)
-    const indexCar = cars.findIndex(car=> car.id === id)
-    cars.splice(indexCar,1)
+    const indexuser = users.findIndex(user=> user.id === id)
+    note.splice(indexuser,1)
     response.status(200).json()
 })
 
